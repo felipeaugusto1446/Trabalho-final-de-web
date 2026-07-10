f82ac20a6d343da3b146c44cc2350a41438ddf77
felipeaugusto1446 <felipeaugusto@makasi.com.br>
2026-07-10 17:06:03 -0300

    feat: corrigindo cors do backend, adicionando formulario de login e integrando com o backend

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from controller import auth_controller
 from controller import enderecos_controller
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="API Meus Endereços", version="1.0.0")
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
 @app.get("/")
 def root():
     return {"mensagem":"API RODANDO COM SUCESSO!!"}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header"
+import { Login } from "./components/Login"
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Header />
+      <Login />
       <h1 className="text-4xl font-bold text-blue-600">
         deu tudo certo
       </h1>
