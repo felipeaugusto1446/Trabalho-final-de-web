166f65fb2b355d95a2d24b20e434f3682eb3f29c
felipeaugusto1446 <felipeaugusto@makasi.com.br>
2026-07-10 15:09:04 -0300

    feat: inserindo rotas no main

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from controller import auth_controller
+from controller import enderecos_controller
 
 app = FastAPI(title="API Meus Endereços", version="1.0.0")
-
-app.include_router(auth_controller.router)
 
 @app.get("/")
 def root():
     return {"mensagem":"API RODANDO COM SUCESSO!!"}
+
+app.include_router(auth_controller.router)
+app.include_router(enderecos_controller.router)
+
+
